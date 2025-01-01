64fc13799c3ae54a3d3cccd5bcbe014389efff78
varodv <varodv@gmail.com>
2025-01-01 18:36:36 +0100

    fix: Complete last commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   test: {
     globals: true,
+    environment: 'jsdom',
     coverage: {
       exclude: [...coverageConfigDefaults.exclude, 'src/**/types'],
     },
