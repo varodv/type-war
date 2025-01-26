78d0357733576c8fc306c386ea8aa761c26666e3
varodv <varodv@gmail.com>
2025-01-26 19:20:17 +0100

    feat: Add Storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import js from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import storybook from 'eslint-plugin-storybook';
 import globals from 'globals';
 import tsEslint from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
   ...tsEslint.configs.recommended,
+  ...storybook.configs['flat/recommended'],
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
